d907dc6e2e106dcbf0f6ac7c097ced0422c36082
dhruv0002 <dhruv0002@gmail.com>
2021-11-08 22:09:04 -0500

    Developing prize information backend

---
+++ Project/Project/Pages/PrizeDetails.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuickTypeNobelLaureates;
+
+namespace Project.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        public bool SearchCompleted { get; set; }
+        public NobelLaureates NobelLaureates { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            SearchCompleted = true;
+
+            if (id == null)
+            {
+                SearchCompleted = false;
+            }
+            else
+            {
+                using (var webClient = new WebClient())
+                {
+                    string jsonString = webClient.DownloadString($"https://api.nobelprize.org/v1/laureate.json?id={id}");
+
+                    NobelLaureates = NobelLaureates.FromJson(jsonString);
+
+                }
+
+                if (NobelLaureates == null)
+                {
+                    SearchCompleted = false;
+                }
+            }
+
+            return Page();
+        }
+    }
+}
