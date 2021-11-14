3056b82839c157bfce94661512bf928ad08a016d
Mian Wang <wangm7@mail.uc.edu>
2021-11-14 18:54:08 -0500

    Tried to add Prize as an individual Page
    but I don't know how to extract Prize

--- Project/Project/Pages/PrizeDetails.cshtml.cs
+++ Project/Project/Pages/PrizeDetails.cshtml.cs
@@ -14,6 +14,28 @@ namespace Project.Pages
         public bool SearchCompleted { get; set; }
         public NobelLaureates NobelLaureates { get; set; }
 
+        public IActionResult onGet()
+        {
+
+        //    using (var webClient = new WebClient())
+        //    {
+        //        string jsonString = webClient.DownloadString("https://api.nobelprize.org/v1/laureate.json?id=");
+
+        //        NobelLaureates = NobelLaureates.FromJson(jsonString);
+
+        //        List<QuickTypeNobelLaureates.Prize> LaureatesListForExtractPrizes = new List<QuickTypeNobelLaureates.Prize>();
+
+        //        foreach (var P in LaureatesListForExtractPrizes)
+        //        {
+        //            ViewData["Year"] = P.Year;
+        //            ViewData["Category"] = P.Category;
+        //            ViewData["Share"] = P.Share;
+        //            ViewData["Motivation"] = P.Motivation;
+        //        }
+        //    }
+        //    return Page();
+        //}
+
         public IActionResult OnGet(int? id)
         {
             SearchCompleted = true;
