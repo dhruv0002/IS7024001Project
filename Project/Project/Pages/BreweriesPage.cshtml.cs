750ee8b7abc484cc47045a241ad745569ef0451d
b-sriya <sriya.boyapally98@gmail.com>
2021-11-29 19:43:35 -0500

    Adding backend functionalities for breweries page

---
+++ Project/Project/Pages/BreweriesPage.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Breweries;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project.Pages
+{
+    public class BreweriesPageModel : PageModel
+    {
+        public List<BreweryCollection> BreweryList { get; set; }
+        public IActionResult OnGet()
+        {
+            
+            using (var webClient = new WebClient())
+            {
+                string jsonString = "";
+                try
+                {
+                    jsonString = webClient.DownloadString("https://breweryarc.azurewebsites.net/breweryentire");
+                    BreweryList = BreweryCollection.FromJson(jsonString);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception occured: ", e);
+                }
+
+            }
+
+            return Page();
+        }
+    }
+}
