970098ebf408196a311ddba26e5bf881a0a8b493
b-sriya <sriya.boyapally98@gmail.com>
2021-11-29 19:39:39 -0500

    Adding constants for events

---
+++ Project/Project/Constants/EventConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Constants
+{
+    public static class EventConstants
+    {
+        public const string Admin = "Admin";
+        public const string ReceiveMessage = "ReceiveMessage";
+        public const string ReceiveQuestion = "ReceiveQuestion";
+        public const string RecieveResult = "RecieveResult";
+        public const string ReceiveScore = "ReceiveScore";
+        public const string StopGame = "StopGame";
+        public const string EnablePlayAgainButton = "EnablePlayAgainButton";
+    }
+}
